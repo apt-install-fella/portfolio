95938afba01f4eadc35e26b8f4864c84770aa2e0
Fella Bennounas <bennounas@unistra.fr>
2026-09-25 00:32:41 +0200

    fix config

--- portfolio-astro/portfolio/astro.config.mjs
+++ portfolio-astro/portfolio/astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 
-// ⚠️ À adapter avant de déployer :
-// - site : ton URL GitHub Pages (ex: "https://fellabennounas.github.io")
-// - base : le nom de ton dépôt précédé d'un slash (ex: "/portfolio")
-//   Si ton dépôt s'appelle exactement "TONPSEUDO.github.io", laisse base à "/"
 export default defineConfig({
-  site: "https://fellabennounas.github.io",
+  site: "https://apt-install-fella.github.io",
   base: "/portfolio",
 });
